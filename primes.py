dd0cbf999fa6749e79241e4337b4b3d9d21f58c3
Yousef Alhujaylan <yousef.alhujaylan@kcl.ac.uk>
2022-09-30 21:23:15 +0100

    Add exception to file.

--- primes.py
+++ primes.py
@@ -1,6 +1,24 @@
 """List of prime numbers generator."""
 """ENTER YOUR SOLUTION HERE!"""
 
+
 def primes(number_of_primes):
+
+    if number_of_primes <= 0:
+        raise ValueError("Number of primes must be larger than 0.")
     list = []
+    num = 2
+    i = 0
+
+    while i < number_of_primes:
+        prime = True
+        for j in range(2, num):
+            if num % j == 0:
+                prime = False
+
+        if prime:
+            list.append(num)
+            i = i + 1
+
+        num = num + 1
     return list
